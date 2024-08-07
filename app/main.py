d1e6524a4e94fde569ce9cdc00e4da851975833f
ZakPorat <zakporat2023@gmail.com>
2024-08-06 23:16:15 -0400

    Created basic scraping function

--- app/main.py
+++ app/main.py
@@ -6,16 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def scroll_down_entire_page(driver: WebDriver, timeout: int):
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
-    time.sleep(timeout)
-
 def scrape_indeed_jobs(position: str, location: str, page_count: int):
     base_url = f"https://www.indeed.com/jobs?q={position}&l={location}"
 
@@ -31,19 +21,21 @@ def scrape_indeed_jobs(position: str, location: str, page_count: int):
     # waiting for the page to load
     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "job_seen_beacon")))
 
-    scroll_down_entire_page(driver, 3) # 3 second timeout after page scroll
+    post_data = set()
 
-    posts = driver.find_elements(By.class_name, "job_seen_beacon")
+    posts = driver.find_elements(By.CLASS_NAME, "job_seen_beacon")
 
-    a_tags = posts.find_elements(By.TAG_NAME, "a")
+    for post in posts:
+        a_tag = post.find_element(By.TAG_NAME, "a")
 
-    for element in a_tags:
-        href = element.get_attribute("href")
-        print(href)
+        href = a_tag.get_attribute("href")
 
-    time.sleep(1)
+        post_data.add(href)
 
     driver.quit()
+
+    print(len(post_data))
+    print(post_data)
 
 def main():
     position = "softwareenginner"
