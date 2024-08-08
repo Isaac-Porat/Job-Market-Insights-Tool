30dcadce365bb0b0efbc97a072354b67f7e3601c
ZakPorat <zakporat2023@gmail.com>
2024-08-08 20:10:27 +0100

    Added caching system, individual job posting scraping, csv data

--- app/main.py
+++ app/main.py
@@ -1,50 +1,161 @@
 import time
+import json
+import os
+from typing import List
+
 import pandas as pd
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
-def scrape_indeed_jobs(position: str, location: str, page_count: int):
+CACHE_FILE = 'job_cache.json'
+
+def load_cache():
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, 'r') as f:
+            return json.load(f)
+    return {}
+
+def save_cache(cache):
+    with open(CACHE_FILE, 'w') as f:
+        json.dump(cache, f)
+
+def extract_vjk(url):
+    vjk = url.split("vjk=")[1]
+    return vjk
+
+def scrape_indeed_jobs(position: str, location: str, page_count: int) -> List:
     base_url = f"https://www.indeed.com/jobs?q={position}&l={location}"
 
     options = Options()
-    # options.add_argument("headless=new")
+    # options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = WebDriver(options=options)
 
-    driver.get(base_url)
-
-    wait = WebDriverWait(driver, 10)
-
-    # waiting for the page to load
-    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "job_seen_beacon")))
-
     post_data = set()
 
-    posts = driver.find_elements(By.CLASS_NAME, "job_seen_beacon")
+    try:
+        driver.get(base_url)
 
-    for post in posts:
-        a_tag = post.find_element(By.TAG_NAME, "a")
+        cache = load_cache()
 
-        href = a_tag.get_attribute("href")
+        wait = WebDriverWait(driver, 10)
 
-        post_data.add(href)
+        # waiting for url to load
+        wait.until(EC.url_contains("vjk="))
 
-    driver.quit()
+        current_url = driver.current_url
 
-    print(len(post_data))
-    print(post_data)
+        print(f"Scraping URL: {current_url}")
+
+        vjk = extract_vjk(current_url)
+
+        if vjk in cache:
+            print(f"{vjk} found in cache, skipping url...")
+            driver.quit()
+            return cache[vjk]
+
+        posts = driver.find_elements(By.CLASS_NAME, "job_seen_beacon")
+
+        for post in posts:
+            a_tag = post.find_element(By.TAG_NAME, "a")
+
+            href = a_tag.get_attribute("href")
+
+            post_data.add(href)
+
+    except Exception as e:
+        print(f"Error scraping {base_url}: {e}")
+
+    finally:
+        driver.quit()
+
+    cache[vjk] = list(post_data)
+    save_cache(cache)
+
+    return list(post_data)
+
+def scrape_job_posts(url: str):
+
+    options = Options()
+    # options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = WebDriver(options=options)
+
+    try:
+        driver.get(url)
+        print(f"Scraping URL: {url}")
+
+        # wait = WebDriverWait(driver, 10)
+
+        try:
+            title = driver.find_element(By.TAG_NAME, "h1").text
+        except NoSuchElementException:
+            title = None
+
+        try:
+            company_name = driver.find_element(By.CLASS_NAME, "jobsearch-CompanyReview--heading")
+            company_name = company_name.get_attribute("innerHTML")
+        except NoSuchElementException:
+            company_name = None
+
+        try:
+            location = driver.find_element(By.CLASS_NAME, "css-waniwe").text
+        except NoSuchElementException:
+            location = None
+
+        try:
+            salary = driver.find_element(By.CLASS_NAME, "css-19j1a75").text
+        except NoSuchElementException:
+            salary = None
+
+        try:
+            description = driver.find_element(By.ID, "jobDescriptionText").text
+        except NoSuchElementException:
+            description = None
+
+    except Exception as e:
+        print(f"Error scraping {url}: {str(e)}")
+        return {}
+
+    finally:
+        driver.quit()
+
+    return {
+        "url": url,
+        "title": title,
+        "company_name": company_name,
+        "location": location,
+        "salary": salary,
+        "description": description
+    }
+
 
 def main():
-    position = "softwareenginner"
+    position = "software+engineer"
     page_count = 1
 
-    data = scrape_indeed_jobs(position, "boston", page_count)
+    job_urls = scrape_indeed_jobs(position, "boston", page_count)
 
-    # df = pd.DataFrame(data, columns=["title", "citation", "description", "url"])
-    # df.to_csv(f"{query}.csv", index=False)
+    post_data = []
+
+    for url in job_urls:
+        job_details = scrape_job_posts(url)
+        post_data.append(job_details)
+
+    df = pd.DataFrame(post_data)
+    df.to_csv(f"{position.replace('+', '_')}.csv", index=False)
 
 if __name__ == "__main__":
     main()
